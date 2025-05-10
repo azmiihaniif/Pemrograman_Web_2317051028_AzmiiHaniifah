30f503b56afbaecbe744a1696025cc612a1fe755
AZMII HANIIFAH <168162631+azmiihaniif@users.noreply.github.com>
2025-05-10 20:44:54 +0700

    Add files via upload

---
+++ Pertemuan 11/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    ?>
+    <form method = "post" action = "tampil.php">
+        <label>Masukkan nama</label>
+        <input type = "text" name = "nama">
+
+        <label>Masukkan usia</label>
+        <input type = "text" name = "usia">
+
+        <label>Masukkan npm</label>
+        <input type = "text" name = "npm">
+
+        <label>Masukkan alamat</label>
+        <input type = "text" name = "alamat">
+
+        <input type = "submit">
+    </form>
+</body>
+</html>
---
+++ Pertemuan 11/tampil.php
@@ -0,0 +1,15 @@
+<?php
+    $nama = $_POST["nama"];
+    $usia = $_POST["usia"];
+    $npm = $_POST["npm"];
+    $alamat = $_POST["alamat"];
+
+    echo "Nama anda adalah " . $nama;
+    echo "<br>";
+    echo "Usia anda adalah " . $usia;
+    echo "<br>";
+    echo "Npm anda adalah " . $usia;
+    echo "<br>";
+    echo "Alamat anda adalah " . $usia;
+    echo "<br>";
+?>
